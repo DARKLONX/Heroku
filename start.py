1cd09885a48643d2c5f5985ffadcb540ef1c93a6
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-14 10:29:42 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from os import system # chala ja 
-system ("git clone https://GitHub.com/DARKLONX/darklonx && cd darklonx && python3 -m ULTRAX")
+system ("git clone https://GitHub.com/DARKLONX/darklonx && cd darklonx && python3 -m userbot")
 # try af
 print("OH SYSTEM IS CRASHED")
-system("python3 -m darklonx")
+system("python3 -m userbot")
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
